22f5030c4a39191c606102f79801b7f34289980e
BuildTools <unconfigured@null.spigotmc.org>
2020-10-03 01:51:25 +0800

    Fixed build's bug.

--- src/main/kotlin/me/scoretwo/fastscript/api/plugin/FastScriptMain.kt
+++ src/main/kotlin/me/scoretwo/fastscript/api/plugin/FastScriptMain.kt
@@ -1,9 +1,6 @@
 package me.scoretwo.fastscript.api.plugin
 
-import com.sun.xml.internal.ws.util.StreamUtils
-import me.scoretwo.fastscript.utils.FileUtils
 import java.io.File
-import java.io.InputStream
 
 interface FastScriptMain {
 
